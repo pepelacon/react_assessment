3ae24c888036e0845ecd8dd3affdabbe813730ba
Daniel <pepelacon@gmail.com>
2025-03-30 15:54:36 -0400

    created ItemsPage/ItemCard components and added external data items

---
+++ src/components/ItemsPage.js
@@ -0,0 +1,5 @@
+const ItemsPage = () => {
+  return <div>Hello</div>;
+};
+
+export default ItemsPage;
---
+++ src/externalBD/BD.js
@@ -0,0 +1,60 @@
+const items = [
+  {
+    name: "Collagen Jelly Cream- Niacinamide",
+    description:
+      "medicube Collagen Jelly Cream- Niacinamide & Freeze-Dried Hydrolyzed Collagen - Boosts skin's barrier hydration and gives 24h Glow & Lifted Look - No artificial color, Korean skincare (3.71 fl.oz.)",
+    price: 14.09,
+    url: "https://m.media-amazon.com/images/I/61leLgR06vL._SX679_.jpg",
+  },
+  {
+    name: "Carbo Gain Powder (Maltodextrin)",
+    description:
+      "PURE MALTODEXTRIN: Maltodextrin is comprised of 'glucose polymers,' which are chains of glucose molecules that are easy for the body to digest and assimilate",
+    price: 35.33,
+    url: "https://m.media-amazon.com/images/I/61DD8MC9pgL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+  },
+  {
+    name: "WILSON AVP Game Volleyballs",
+    description:
+      "ENHANCED BALL TRACKING: The vibrant color palette and Optic Flow graphics allow for better ball tracking in the dynamic and diverse environments of beach volleyball",
+    price: 23.49,
+    url: "https://m.media-amazon.com/images/I/71al-5wkasL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+  },
+  {
+    name: "3 Pack Men's Muscle Dry Fit Compression Tank Top",
+    description:
+      "Great Performance in warm or cool weather. Sweat and Moisture can be pulled away from skin very quickly; Enable sweat easy for air-dry to prevent clothes sticking to the body.",
+    price: 19.99,
+    url: "https://m.media-amazon.com/images/I/71duHfXwxUL._AC_SX679_.jpg",
+  },
+  {
+    name: "Sensodyne Pronamel Intensive Repair Toothpaste",
+    description:
+      "Four 3.4 oz tubes of arctic breeze Sensodyne Pronamel Intensive Enamel Repair Toothpaste for Sensitive Teeth and Cavity Protection, Whitening Toothpaste to Strengthen Enamel",
+    price: 22.13,
+    url: "https://m.media-amazon.com/images/I/51EZlgV2OIL._SX300_SY300_QL70_FMwebp_.jpg",
+  },
+  {
+    name: "DEWALT Diamond Blade for Masonry",
+    description:
+      "HYDRATION MIX: A great-tasting, non-GMO electrolyte drink mix that delivers hydration faster than water alone",
+    price: 23.63,
+    url: "https://m.media-amazon.com/images/I/710YcPpwT+L._SX522_.jpg",
+  },
+  {
+    name: "Liquid I.V.® Hydration - Popsicle Firecracker ",
+    description:
+      "SCIENCE-BACKED FORMULA: Powered by LIV HYDRASCIENCE, Liquid I.V. is designed with an optimized ratio of electrolytes and clinically tested nutrients",
+    price: 19.99,
+    url: "https://m.media-amazon.com/images/I/812KjGBLVBL._AC_SX679_.jpg",
+  },
+  {
+    name: "Tide Simply Clean & Fresh Liquid Laundry Detergent",
+    description:
+      "2X the power of baking soda detergent* (*Stain removers of 1 dose vs. 2 doses of the leading detergent with baking soda in a standard, top-loading machine)",
+    price: 10.44,
+    url: "https://m.media-amazon.com/images/I/81V2JBgsSVL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+  },
+];
+
+export default items;
